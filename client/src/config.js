42937deba42b6b89b2981248ec5c1a36b214e5a3
alexisTobar <167795768+alexisTobar@users.noreply.github.com>
2025-12-11 17:33:47 -0300

    api render
    Changed BACKEND_URL to a fixed Render URL.

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,6 @@
 // client/src/config.js
 
-// VITE_BACKEND_URL es la variable que configuraste en el panel de Vercel.
-// Si esa variable existe, la usamos. Si no, usamos localhost.
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
+// FORZAMOS la URL de Render para que no tenga opción de fallar.
+const BACKEND_URL = "https://deck-myl.onrender.com";
 
 export default BACKEND_URL;
