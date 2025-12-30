4ec3d68edd90ef7224aa419d7a08c3f472961d9d
alexisTobar <tobaralexis.89@gmail.com>
2025-12-29 21:28:56 -0300

    inspeccion

--- client/src/pages/ImperioHome.jsx
+++ client/src/pages/ImperioHome.jsx
@@ -228,6 +228,7 @@ export default function ImperioBuilder() {
                                     </div>
                                 ))}
                             </div>
+                            
                         </div>
                     ))}
                 </div>
